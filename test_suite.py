7e5c571b5e0b2a1db915ff5a257f800090674795
unknown <you@example.com>
2025-06-14 18:29:04 +0100

    update

--- test_suite.py
+++ test_suite.py
@@ -7,7 +7,7 @@ from Config.configuration import Config
 @pytest.fixture(scope="session")
 def driver_setup():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")      # Run in headless mode
+    #chrome_options.add_argument("--headless")      # Run in headless mode
     chrome_options.add_argument("--disable-gpu")   # Prevent GPU errors in headless mode
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(20)                     # Wait implicitly up to 20s
@@ -36,7 +36,7 @@ def test_customer_form_page(login):
     customer_form.enter_firstname(Config.FIRST_NAME)
     customer_form.enter_lastname(Config.LAST_NAME)
     customer_form.enter_city(Config.CITY)
-    customer_form.enter_state(Config.STATE)
+    # customer_form.enter_state(Config.STATE)
     customer_form.click_gender()
     customer_form.tick_add_to_promotional_list()
     customer_form.click_submit()
